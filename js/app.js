308b4127854e835c9c9a5cdb1f33b2d69f462d36
villalobos23 <villalobos23@gmail.com>
2015-12-15 22:13:46 -0430

    removidos campos que seran auto generados por la aplicacion real, Cambios en el html para organizar y agrupar mejor los valores, y se agrega el js que manejara validaciones, se empieza con el campo de edicion de registro

--- js/app.js
+++ js/app.js
@@ -1,5 +1,19 @@
 /* Application Sripts */
 
+function showSearchControl(){
+  var searchControl = document.getElementById("nro_solicitud"),
+      requestType = this;
+  if(this.value == 1){
+    searchControl.className = "col-md-6 hidden";
+  }else{
+    searchControl.className = "col-md-6";
+  }
+}
+
 $(function () {
   'use strict';
+  
+  //mostrar campo cuando se quiera editar
+  var requestType = document.getElementById("tipo_solicitud");    
+  requestType.addEventListener("change",showSearchControl,false);
 });
